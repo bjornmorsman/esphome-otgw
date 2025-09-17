5a093df7b27ed304aa63f77fe55f90bb0911484c
bjornmorsman <81650430+bjornmorsman@users.noreply.github.com>
2025-09-17 17:28:06 +0200

    Update __init__.py

--- components/otgw/climate/__init__.py
+++ components/otgw/climate/__init__.py
@@ -15,24 +15,22 @@ OpenThermGatewayClimateThermostat = otgw_ns.class_(
 CONF_TARGET_TEMPERATURE_CONSTANT = "target_temperature_constant"
 CONF_EXTERNAL_TEMPERATURE_SENSOR = "external_temperature_sensor"
 
-# Dit is het juiste platform schema met externe sensor
 PLATFORM_SCHEMA = climate.CLIMATE_PLATFORM_SCHEMA.extend({
     cv.Optional(CONF_TARGET_TEMPERATURE_CONSTANT, default=False): cv.boolean,
     cv.Optional(CONF_EXTERNAL_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
 })
 
-
 async def to_code(config):
-    # Maak de climate component aan
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
 
-    # Target temperature constant
     if CONF_TARGET_TEMPERATURE_CONSTANT in config:
         cg.add(var.set_target_temperature_constant(config[CONF_TARGET_TEMPERATURE_CONSTANT]))
 
-    # Externe sensor
     if CONF_EXTERNAL_TEMPERATURE_SENSOR in config:
         ext_sensor = await cg.get_variable(config[CONF_EXTERNAL_TEMPERATURE_SENSOR])
         cg.add(var.set_external_sensor(ext_sensor))
+
+# Dit is cruciaal: platform registreren zodat ESPHome 'id', 'name', etc. herkent
+climate.register_climate_platform("otgw", PLATFORM_SCHEMA, to_code)
