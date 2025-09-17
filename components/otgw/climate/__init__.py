a40fb5c8124c26dc24b35c2e2ce80ed2e8684706
bjornmorsman <81650430+bjornmorsman@users.noreply.github.com>
2025-09-17 17:25:51 +0200

    Update __init__.py

--- components/otgw/climate/__init__.py
+++ components/otgw/climate/__init__.py
@@ -12,30 +12,27 @@ OpenThermGatewayClimateThermostat = otgw_ns.class_(
     "OpenThermGatewayClimateThermostat", climate.Climate, cg.Component
 )
 
-SENSOR_ROOM_THERMOSTAT = "room_thermostat"
 CONF_TARGET_TEMPERATURE_CONSTANT = "target_temperature_constant"
 CONF_EXTERNAL_TEMPERATURE_SENSOR = "external_temperature_sensor"
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(CONF_OTGW_ID): cv.use_id(OpenThermGateway),
-
-    cv.Optional(SENSOR_ROOM_THERMOSTAT): climate.climate_schema(OpenThermGatewayClimateThermostat).extend({
-        cv.GenerateID(): cv.declare_id(OpenThermGatewayClimateThermostat),
-        cv.Optional(CONF_TARGET_TEMPERATURE_CONSTANT, default=False): cv.boolean,
-        cv.Optional(CONF_EXTERNAL_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
-    }),
+# Dit is het juiste platform schema met externe sensor
+PLATFORM_SCHEMA = climate.CLIMATE_PLATFORM_SCHEMA.extend({
+    cv.Optional(CONF_TARGET_TEMPERATURE_CONSTANT, default=False): cv.boolean,
+    cv.Optional(CONF_EXTERNAL_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
 })
 
+
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_OTGW_ID])
+    # Maak de climate component aan
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
 
-    if SENSOR_ROOM_THERMOSTAT in config:
-        var = cg.new_Pvariable(config[SENSOR_ROOM_THERMOSTAT][CONF_ID])
-        await cg.register_component(var, config[SENSOR_ROOM_THERMOSTAT])
-        await climate.register_climate(var, config[SENSOR_ROOM_THERMOSTAT])
-        cg.add(var.set_parent(parent))
-        cg.add(var.set_target_temperature_constant(config[SENSOR_ROOM_THERMOSTAT][CONF_TARGET_TEMPERATURE_CONSTANT]))
+    # Target temperature constant
+    if CONF_TARGET_TEMPERATURE_CONSTANT in config:
+        cg.add(var.set_target_temperature_constant(config[CONF_TARGET_TEMPERATURE_CONSTANT]))
 
-        if CONF_EXTERNAL_TEMPERATURE_SENSOR in config[SENSOR_ROOM_THERMOSTAT]:
-            ext_sensor = await cg.get_variable(config[SENSOR_ROOM_THERMOSTAT][CONF_EXTERNAL_TEMPERATURE_SENSOR])
-            cg.add(var.set_external_sensor(ext_sensor))
+    # Externe sensor
+    if CONF_EXTERNAL_TEMPERATURE_SENSOR in config:
+        ext_sensor = await cg.get_variable(config[CONF_EXTERNAL_TEMPERATURE_SENSOR])
+        cg.add(var.set_external_sensor(ext_sensor))
