98018f95e05f7dfcf9a5d7c6ba1e8ffa80b2e397
bjornmorsman <81650430+bjornmorsman@users.noreply.github.com>
2025-09-17 17:00:24 +0200

    Update climate.h

--- components/otgw/climate/climate.h
+++ components/otgw/climate/climate.h
@@ -2,53 +2,31 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/climate/climate.h"
-#include "esphome/components/sensor/sensor.h"  // nodig voor externe sensor
 #include "esphome/components/otgw/otgw.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace otgw {
 
-// Keuze van bron voor kamertemperatuur
-enum class RoomTemperatureSource {
-  OTGW_THERMOSTAT,
-  EXTERNAL_SENSOR,
-};
-
 class OpenThermGatewayClimateThermostat : public climate::Climate, public Component {
- public:
-  void setup() override;
-  void dump_config() override;
-  climate::ClimateTraits traits() override;
-  void control(const climate::ClimateCall& call) override;
-  void loop() override;  // voor externe sensor
+public:
+    void setup() override;
+    void dump_config() override;
+    climate::ClimateTraits traits() override;
+    void control(const climate::ClimateCall& call) override;
 
-  void set_parent(OpenThermGateway *parent) { this->parent_ = parent; }
-  void set_target_temperature_constant(bool constant) { this->target_temperature_constant_ = constant; }
+    void set_parent(OpenThermGateway *parent) { this->parent_ = parent; }
+    void set_target_temperature_constant(bool constant) { this->target_temperature_constant_ = constant; }
+    void set_external_sensor(sensor::Sensor *sensor) { this->external_sensor_ = sensor; }
 
-  void set_room_temperature_source(RoomTemperatureSource src) { this->room_temp_source_ = src; }
-  void set_external_room_sensor(sensor::Sensor *sensor) { this->external_room_sensor_ = sensor; }
+protected:
+    OpenThermGateway *parent_;
+    bool target_temperature_constant_;
+    sensor::Sensor *external_sensor_{nullptr};
 
-  void set_room_temperature_source_from_string(const std::string &src_str) {
-    if (src_str == "otgw") {
-      this->room_temp_source_ = RoomTemperatureSource::OTGW_THERMOSTAT;
-    } else if (src_str == "external") {
-      this->room_temp_source_ = RoomTemperatureSource::EXTERNAL_SENSOR;
-    } else {
-      ESP_LOGW("otgw.climate", "Unknown room_temperature_source: %s, defaulting to OTGW", src_str.c_str());
-      this->room_temp_source_ = RoomTemperatureSource::OTGW_THERMOSTAT;
-    }
-  }
-
- protected:
-  OpenThermGateway *parent_;
-  bool target_temperature_constant_{false};
-
-  RoomTemperatureSource room_temp_source_{RoomTemperatureSource::OTGW_THERMOSTAT};
-  sensor::Sensor *external_room_sensor_{nullptr};
-
-  void on_otmessage(const OpenThermMessage &message);
-  void on_timeout();
+    void on_otmessage(const OpenThermMessage &message);
+    void on_timeout();
 };
 
-}  // namespace otgw
-}  // namespace esphome
+} // namespace otgw
+} // namespace esphome
