fffefac2da91afb11e4d2bb3af2c776808778104
bjornmorsman <81650430+bjornmorsman@users.noreply.github.com>
2025-09-17 17:20:44 +0200

    Update __init__.py

--- components/otgw/climate/__init__.py
+++ components/otgw/climate/__init__.py
@@ -2,7 +2,11 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, sensor
 from esphome.const import CONF_ID
+
 from .. import otgw_ns, CONF_OTGW_ID, OpenThermGateway
+
+DEPENDENCIES = ["otgw"]
+CODEOWNERS = ["@mvdnes"]
 
 OpenThermGatewayClimateThermostat = otgw_ns.class_(
     "OpenThermGatewayClimateThermostat", climate.Climate, cg.Component
