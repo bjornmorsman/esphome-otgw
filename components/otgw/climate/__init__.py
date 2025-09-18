22288a2c6878b861fc716a1d2f4366c214e3f942
bjornmorsman <81650430+bjornmorsman@users.noreply.github.com>
2025-09-18 05:02:02 +0200

    Update __init__.py

--- components/otgw/climate/__init__.py
+++ components/otgw/climate/__init__.py
@@ -19,23 +19,26 @@ CONF_EXTERNAL_TEMPERATURE_SENSOR = "external_temperature_sensor"
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_OTGW_ID): cv.use_id(OpenThermGateway),
 
-    cv.Optional(SENSOR_ROOM_THERMOSTAT): climate.climate_schema(OpenThermGatewayClimateThermostat).extend({
+    cv.Optional(SENSOR_ROOM_THERMOSTAT): cv.Schema({
         cv.GenerateID(): cv.declare_id(OpenThermGatewayClimateThermostat),
         cv.Optional(CONF_TARGET_TEMPERATURE_CONSTANT, default=False): cv.boolean,
         cv.Optional(CONF_EXTERNAL_TEMPERATURE_SENSOR): cv.use_id(sensor.Sensor),
-    }),
+    }).extend(climate.CLIMATE_SCHEMA),
 })
+
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_OTGW_ID])
 
     if SENSOR_ROOM_THERMOSTAT in config:
-        var = cg.new_Pvariable(config[SENSOR_ROOM_THERMOSTAT][CONF_ID])
-        await cg.register_component(var, config[SENSOR_ROOM_THERMOSTAT])
-        await climate.register_climate(var, config[SENSOR_ROOM_THERMOSTAT])
-        cg.add(var.set_parent(parent))
-        cg.add(var.set_target_temperature_constant(config[SENSOR_ROOM_THERMOSTAT][CONF_TARGET_TEMPERATURE_CONSTANT]))
+        conf = config[SENSOR_ROOM_THERMOSTAT]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        await climate.register_climate(var, conf)
 
-        if CONF_EXTERNAL_TEMPERATURE_SENSOR in config[SENSOR_ROOM_THERMOSTAT]:
-            ext_sensor = await cg.get_variable(config[SENSOR_ROOM_THERMOSTAT][CONF_EXTERNAL_TEMPERATURE_SENSOR])
+        cg.add(var.set_parent(parent))
+        cg.add(var.set_target_temperature_constant(conf[CONF_TARGET_TEMPERATURE_CONSTANT]))
+
+        if CONF_EXTERNAL_TEMPERATURE_SENSOR in conf:
+            ext_sensor = await cg.get_variable(conf[CONF_EXTERNAL_TEMPERATURE_SENSOR])
             cg.add(var.set_external_sensor(ext_sensor))
